6c9dcaa643e912e291d26dee0a7e09d78d6740c5
Rick Naro <sillygoose@me.com>
2021-09-06 00:42:17 -0400

    Journeys and detailed journey data is accessible

--- fordconnect/myjourneys.py
+++ fordconnect/myjourneys.py
@@ -9,6 +9,7 @@
 import logging
 import sys
 import requests
+import random
 
 import version
 import logfiles
@@ -52,6 +53,95 @@ def get_journeys(start, end):
     return status
 
 
+def get_journey_details(id):
+
+    global _VEHICLECLIENT
+
+    status = None
+    tries = 3
+    while tries > 0:
+        try:
+            status = _VEHICLECLIENT.journey_details(id=id)
+            break
+        except requests.ConnectionError:
+            tries -= 1
+            # _LOGGER.info(f"request.get() timed out, {tries} remaining")
+            if tries == 0:
+                _LOGGER.error(f"FordPass Connect API unavailable")
+                raise
+            continue
+        except Exception as e:
+            _LOGGER.error(f"Unexpected exception: {e}")
+            break
+    return status
+
+
+def get_street_town(location):
+    components = location["results"][0]["address_components"]
+    return f"'{components['street']} {components['suffix']}, {components['city']}'"
+
+
+def display_journey_details(journey):
+    journeyID = journey.get("journeyID")
+    details = get_journey_details(id=journeyID)
+    _LOGGER.info(f"")
+    _LOGGER.info(f"Journey details for {journeyID}")
+
+    duration = details["value"].get("summary").get("duration")
+    hours = duration // 3600
+    minutes = (duration // 60) % 60
+
+    distance = details["value"].get("summary").get("distance") / 1000
+    distance_unit = "km"
+    if _MILES:
+        distance = distance * 0.6214
+        distance_unit = "miles"
+
+    avgSpeed = 3600 / 1000 * details["value"].get("summary").get("avgSpeed")
+    speed_unit = "kph"
+    if _MILES:
+        avgSpeed = avgSpeed * 0.6214
+        speed_unit = "mph"
+
+    _LOGGER.info(
+        f"Duration {hours:.0f} hours, {minutes:.0f} minutes, Distance {distance:.2f} {distance_unit}, Average Speed {avgSpeed:.2f} {speed_unit}"
+    )
+
+
+def display_journey(journey):
+    _LOGGER.info(f"Journey {journey.get('journeyID')}")
+
+    startingAt = journey.get("start")
+    endingAt = journey.get("end")
+
+    startTime = datetime.fromtimestamp(startingAt.get("timestamp"))
+    endTime = datetime.fromtimestamp(endingAt.get("timestamp"))
+    duration = endTime - startTime
+    hours = duration.seconds // 3600
+    minutes = (duration.seconds // 60) % 60
+    _LOGGER.info(
+        f"From {startTime.date()} {startTime.time()} to {endTime.date()} {endTime.time()}, {hours} hours, {minutes} minutes"
+    )
+
+    distance = journey.get("distance") / 1000
+    unit = "km"
+    if _MILES:
+        distance = distance * 0.6214
+        unit = "miles"
+    _LOGGER.info(f"Distance {distance:.2f} {unit}")
+
+    avgSpeed = 3600 / 1000 * journey.get("avgSpeed")
+    unit = "kph"
+    if _MILES:
+        avgSpeed = avgSpeed * 0.6214
+        unit = "mph"
+    _LOGGER.info(f"Average Speed {avgSpeed:.2f} {unit}")
+
+    startingLocationInfo = _GEOCLIENT.reverse((startingAt.get("latitude"), startingAt.get("longitude")))
+    endingLocationInfo = _GEOCLIENT.reverse((endingAt.get("latitude"), endingAt.get("longitude")))
+    _LOGGER.info(f"From {get_street_town(startingLocationInfo)} to {get_street_town(endingLocationInfo)}")
+
+
 def main():
     """Set up and start FordPass Connect."""
 
@@ -76,63 +166,11 @@ def main():
     end_date = datetime.now()
     start_date = end_date - week_ago
     journeys = get_journeys(start=int(start_date.timestamp()), end=int(end_date.timestamp()))
-    for journey in journeys.get("value"):
-        _LOGGER.info(f"Journey {journey.get('journeyID')}")
 
-        startingAt = journey.get("start")
-        endingAt = journey.get("end")
-
-        startTime = datetime.fromtimestamp(startingAt.get("timestamp"))
-        endTime = datetime.fromtimestamp(endingAt.get("timestamp"))
-        duration = endTime - startTime
-        hours = duration.seconds // 3600
-        minutes = (duration.seconds // 60) % 60
-        _LOGGER.info(
-            f"From {startTime.date()} {startTime.time()} to {endTime.date()} {endTime.time()}, {hours} hours, {minutes} minutes"
-        )
-
-        distance = journey.get("distance") / 1000
-        unit = "km"
-        if _MILES:
-            distance = distance * 0.6214
-            unit = "miles"
-        _LOGGER.info(f"Distance {distance:.2f} {unit}")
-
-        avgSpeed = 3600 / 1000 * journey.get("avgSpeed")
-        unit = "kph"
-        if _MILES:
-            avgSpeed = avgSpeed * 0.6214
-            unit = "mph"
-        _LOGGER.info(f"Average Speed {avgSpeed:.2f} {unit}")
-
-        locationInfo = _GEOCLIENT.reverse((startingAt.get("latitude"), startingAt.get("longitude")))
-        nearestLocation = locationInfo.get("results")
-        startingLocation = nearestLocation[0]
-
-        locationInfo = _GEOCLIENT.reverse((endingAt.get("latitude"), endingAt.get("longitude")))
-        nearestLocation = locationInfo.get("results")
-        endingLocation = nearestLocation[0]
-
-        _LOGGER.info(f"From {startingLocation.get('formatted_address')} to {endingLocation.get('formatted_address')}")
-        for location in journey.get("locations"):
-            locationTime = datetime.fromtimestamp(location.get("timestamp"))
-
-            speed = 3600 / 1000 * location.get("speed")
-            unit = "kph"
-            if _MILES:
-                speed = speed * 0.6214
-                unit = "mph"
-
-            locationInfo = _GEOCLIENT.reverse((location.get("latitude"), location.get("longitude")))
-            nearestLocation = locationInfo.get("results")
-            spotLocation = nearestLocation[0]
-
-            _LOGGER.info(
-                f"{locationTime.date()} {locationTime.time()}, speed {speed:.2f} {unit}, {spotLocation.get('formatted_address')}"
-            )
-            pass
-
-        pass
+    journeyList = journeys.get("value")
+    randomJourney = random.randint(0, len(journeyList) - 1)
+    display_journey(journey=journeyList[randomJourney])
+    display_journey_details(journey=journeyList[randomJourney])
 
 
 if __name__ == "__main__":
